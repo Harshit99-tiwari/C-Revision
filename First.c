ea837f43d690b362b348edbc21542eea964b50f6
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-03-22 12:43:37 +0530

    Demonstration of c programming

---
+++ First.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is my demo coding\n");
+    int num = 42;
+    printf("The number is: %d\n", num);
+    float pi = 3.14159;
+    printf("value of pi : %.2f\n", pi);
+    return 0;
+}
