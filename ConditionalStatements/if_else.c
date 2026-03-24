19a7ab3a199a4436d97580ceadd438e1ffbeae2b
Harshit99-tiwari <tiwariankush656@gmail.com>
2026-03-24 22:46:50 +0530

    added if else example

---
+++ ConditionalStatements/if_else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    if(num==2)
+    {
+        printf("even number\n");
+    }
+    else
+    {
+     printf("odd number\n");
+    }
+    return 0;
+}
